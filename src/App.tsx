52aaefd1b8b93bdacfb4e3f22e07bed08ea54c39
WCatgrl <72701420+WCatgrl@users.noreply.github.com>
2025-05-28 10:44:34 -0700

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { TooltipProvider } from "@/components/ui/tooltip";
 import RhymeTest from "@/pages/rhyme-test";
 import NotFound from "@/pages/not-found";
 
@@ -17,9 +16,7 @@ function Router() {
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <Router />
-      </TooltipProvider>
+      <Router />
     </QueryClientProvider>
   );
 }
